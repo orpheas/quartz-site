a322b098114d43440035caaecd371b75f7de1cd1
Orpheas <orpheas@orpheas.io>
2025-12-01 17:29:07 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "www.orpheas.computer",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
